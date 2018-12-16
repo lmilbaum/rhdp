41d4192b0e3dd7aeffb4e3d520ac9b7ad189971c
Liora Milbaum <lmilbaum@redhat.com>
2018-12-16 18:06:56 +0200

    RHEL support

--- minishift/bootstrap.sh
+++ minishift/bootstrap.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-minishift delete --force
-brew cask uninstall minishift
-brew cask install minishift
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if [ -x "$(command -v minishift)" ]; then
+    minishift delete --force
+    brew cask uninstall minishift
+  fi
+  brew cask install minishift
+# else
+#   curl -X GET https://api.github.com/repos/minishift/minishift/releases/latest
+fi
 
 minishift start
 
 eval $(minishift oc-env)
 oc login -u developer -p developer --insecure-skip-tls-verify=true
-oc get -o yaml --export template/jenkins-ephemeral -n openshift | oc create -f -
+oc get -o yaml --export template/jenkins-ephemeral -n openshift | \
+  oc process -f - | oc create -f -
+oc describe dc/jenkins
